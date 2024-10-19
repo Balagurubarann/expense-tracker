98fec8381030beb3b290d014a752c80cc49aa656
Balagurubarann <balagurubaran2002@gmail.com>
2024-10-19 18:38:10 +0530

    modified files

---
+++ backend/controllers/controller.js
@@ -0,0 +1,17 @@
+const Expense = require('../models/expense.model');
+const Income = require('../models/income.model');
+
+exports.getTransactions = async (req, res, next) => {
+    
+    try {
+    
+        const income = await Income.find();
+        const expense = await Expense.find();
+        
+        return res.status(200).json({income, expense});
+
+    } catch (err) {
+        next(err);
+    }
+
+}
--- backend/routes/route.js
+++ backend/routes/route.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 // CONTROLLERS
 const incomeController = require('../controllers/income.controller');
 const expenseController = require('../controllers/expense.controller');
+const controller = require('../controllers/controller');
 
 // INCOME ROUTER
 router
@@ -17,5 +18,8 @@ router
     .get('/home/expenses', expenseController.get_expense)
     .put('/home/update-expense/:id', expenseController.update_expense)
     .delete('/home/delete-expense/:id', expenseController.delete_expense);
+
+router
+    .get('/home/transactions', controller.getTransactions);
 
 module.exports = router;
