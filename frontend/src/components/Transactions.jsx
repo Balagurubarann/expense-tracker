31e34be63120b648fda91ebf4a6b8e8fca31b3a8
Balagurubarann <balagurubaran2002@gmail.com>
2024-10-22 23:04:56 +0530

    files modified

--- frontend/src/components/Transactions.jsx
+++ frontend/src/components/Transactions.jsx
@@ -2,27 +2,64 @@ import { useState, useEffect } from "react";
 
 function Transactions() {
 
-    const [transactions, setTransactions] = useState([]);
+    const [userExpense, setUserExpense] = useState([]);
+    const [userIncome, setUserIncome] = useState([]);
+
+    async function fetchExpense() {
+
+        try {
+
+        const response = await fetch('http://127.0.0.1:1337/home/expenses/');
+        const result = await response.json();
+
+        const { expense } = result;
+
+        setUserExpense(expense);
+
+        console.log(expense);
+
+        } catch (err) {
+            throw err;
+        }
+
+    }
+
+    async function fetchIncome() {
+
+        try {
+
+        const response = await fetch('http://127.0.0.1:1337/home/incomes/');
+        const result = await response.json();
+
+        const { incomes } = result;
+
+        setUserIncome(incomes);
+
+        console.log(incomes);
+
+        } catch (err) {
+            throw err;
+        }
+
+    }
 
     useEffect(() => {
 
-         fetch('http://127.0.0.1:1337/home/expenses/')
-
-            .then(response => response.json())
-            .then(data => {
-                const { expense } = data;
-
-            })
-            .catch(err => {
-                console.log(err);
-            })
+        fetchExpense();
+        fetchIncome();
 
     }, [])
 
     return  <>
                 <p>
                     User Transactions
-                    { transactions }
+
+                    {
+                        userExpense.map((exp, index) => (
+                            <li key={index}>{exp.amount}</li>
+                        ))
+                    }
+
                 </p>
             </>
 
